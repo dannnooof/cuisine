91217640ca2fb73bc7be802ba92c16dc49694f03
dannnooof <kazarindaniil4@gmail.com>
2025-05-26 17:13:21 +0900

    Add files via upload

---
+++ style/style/scrol/scrol.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var navbar = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= navbar) {
+    navbar.classList.add("navbar")
+  } else {
+    navbar.classList.remove("navbar");
+  }
+}
